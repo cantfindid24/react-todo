7c59d1fb822826690355af1247416a5a75c5d9e0
cantfindid24 <106964984+cantfindid24@users.noreply.github.com>
2024-02-03 16:57:16 +0530

    todolist component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { CssBaseline } from '@mui/material';
+import TodoList from './TodoList';
 function App() {
   return (
     <>
       <CssBaseline />
       <h1>Hello</h1>
+      <TodoList />
     </>
   );
 }
